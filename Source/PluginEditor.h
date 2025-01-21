bde91ab6dbbe03c9e4e3db748a8f831f5cdb5939
Yinuo Wu <scott.wyn1215@gmail.com>
2025-01-21 11:47:47 -0500

    Transforming UI to LookAndFeel

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -12,21 +12,39 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
-struct CustomRotarySlider: juce::Slider
+struct LookAndFeel: juce::LookAndFeel_V4
 {
-    CustomRotarySlider(): juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag, juce::Slider::TextEntryBoxPosition::TextBoxBelow)
-    {
-        
-    };
+    void drawRotarySlider (juce::Graphics&,
+                                    int x, int y, int width, int height,
+                                    float sliderPosProportional,
+                                    float rotaryStartAngle,
+                                    float rotaryEndAngle,
+                           juce::Slider&) override {}
 };
 
-struct CustomHorizontalSlider: juce::Slider
+struct RotarySliderWithLabels: juce::Slider
 {
-    CustomHorizontalSlider(): juce::Slider(juce::Slider::SliderStyle::LinearHorizontal,
-                                           juce::Slider::TextEntryBoxPosition::TextBoxBelow)
+    RotarySliderWithLabels(juce::RangedAudioParameter& rap, const juce::String& unitSuffix): juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag, juce::Slider::TextEntryBoxPosition::NoTextBox),
+        param(&rap),
+        suffix(unitSuffix)
     {
-
+        setLookAndFeel(&lnf);
     };
+    
+    ~RotarySliderWithLabels()
+    {
+        setLookAndFeel(nullptr);
+    }
+    
+    void paint(juce::Graphics& g) override {};
+    juce::Rectangle<int> getSliderBounds() const;
+    int getTextHeight() const {return 14;}
+    juce::String getDisplayString() const;
+private:
+    LookAndFeel lnf;
+    
+    juce::RangedAudioParameter* param;
+    juce::String suffix;
 };
 
 struct ResponseCurveComponent: juce::Component, juce::AudioProcessorParameter::Listener, juce::Timer
@@ -66,13 +84,13 @@ private:
     // access the processor object that created it.
     _3BandMultiEffectorAudioProcessor& audioProcessor;
     
-    CustomRotarySlider peakFreqSlider,
+    RotarySliderWithLabels peakFreqSlider,
                         peakGainSlider,
                         peakQualitySlider,
                         lowCutFreqSlider,
-                        highCutFreqSlider;
-
-    CustomHorizontalSlider lowCutSlopeSlider, highCutSlopeSlider;
+                        highCutFreqSlider,
+                        lowCutSlopeSlider,
+                        highCutSlopeSlider;
     
     ResponseCurveComponent responseCurveComponent;
     
